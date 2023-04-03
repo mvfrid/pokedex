9e6f1fbf672ac410b4fd99dd8fbeab9a721c11d0
mvfrid <104832335+mvfrid@users.noreply.github.com01>
2023-04-03 10:37:57 +0200

    testing

---
+++ code/Links.js
@@ -0,0 +1,12 @@
+// https://snack.expo.dev/@mvfrid/matildas-pokeapp
+
+import React from 'react';
+
+export default function Links() {
+  
+  return (
+  <View>
+    <Text>Hello</Text>
+    </View>
+  );
+}
