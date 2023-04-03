e54cdab60ae6552037875dfe319dfa230b8cb656
Frid Matilda <matilda.x.frid@scania.com>
2023-04-03 11:17:16 +0200

    test

--- code/index.js
+++ code/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { Links } from './Links';
+import Links from './Links';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
