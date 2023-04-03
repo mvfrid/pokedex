cbeb2c7c09c020c8362b2df7d469328f387aae04
Frid Matilda <matilda.x.frid@scania.com>
2023-04-03 11:51:12 +0200

    test

--- code/Links.js
+++ code/Links.js
@@ -6,7 +6,16 @@ export default function Links() {
   
   return (
   <>
-  <div><p>Hello there!</p></div>
+  <div class="links"> 
+    <p>To open this app, you need to first download the app Expo Go.</p>
+    <a class="fakeBtn" href="exp://exp.host/@mvfrid/matildas-pokeapp+zAAZ1c9o5m">Open Pokédex App in Expo Go</a>
+    <a class="fakeBtn" href="#">Download Expo Go</a>
+  </div>
+
+  <div class="QR">
+    <p>You can also scan this QR code to open it:</p>
+    <img src="./assets/qr_code.png" />
+  </div>
   </>
   );
 }
